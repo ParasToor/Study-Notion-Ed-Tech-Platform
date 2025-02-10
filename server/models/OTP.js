2f2c78cba0e68db868cc08ba1e38a1a477fd0df9
Paras Toor <parasindersing@gmail.com>
2025-02-10 17:16:56 +0530

    Created a middleware to send OTP for the verification of the mail

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongooose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema(
   {
@@ -15,5 +16,26 @@ const OTPSchema = new mongoose.Schema(
     timeStamps: true,
   }
 );
+
+async function sendVerificationEmail(email, otp) {
+  try {
+    const response = mailSender(
+      email,
+      "Verification Email from Study Notion",
+      otp
+    );
+    console.log("Email sent successfully with response - ", response);
+  } catch (err) {
+    console.error(err);
+    console.log(
+      "Error occured in sendVerificationEmail function placed in OTP model"
+    );
+  }
+}
+
+OTPSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
+});
 
 module.exports = mongoose.model("OTP", OTPSchema);
