d1e4e77755cc3ed5d543c65b67379b6beab273e3
Paras Toor <parasindersing@gmail.com>
2025-02-16 15:41:29 +0530

    npm i cloudinary

---
+++ server/controllers/Course.js
@@ -0,0 +1,15 @@
+const Course = require("../models/Course");
+const Tag = require("../models/Tag");
+const User = require("../models/User");
+
+const createCourse = async (req, res) => {
+  try {
+    a;
+  } catch (err) {
+    a;
+  }
+};
+
+
+
+module.exports = {createCourse}
---
+++ server/utils/imageUploader.js
@@ -0,0 +1,8 @@
+const cloudinary = require("cloudinary").v2;
+
+const uploadImageToCloudinary = async (file, folder, height, quality) => {
+  try {
+  } catch (err) {}
+};
+
+module.exports = uploadImageToCloudinary;
